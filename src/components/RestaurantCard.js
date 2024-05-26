2b077eead7b89b0fe60f055577c7186dca2d3ffa
Ashutosh Dubey <ashd1014@hotmail.com>
2024-05-26 11:43:40 +0530

    Added restaurant card image data in constant

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,9 +1,11 @@
+import { CARD_IMAGE_URL } from "../utils/contants";
+
 const RestaurantCard = ({ restuarantInfo }) => {
   const { name, cuisines, avgRating, costForTwo, deliveryTime } =
     restuarantInfo;
   return (
     <div className="res-card">
-      <img src="https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg" />
+      <img src={CARD_IMAGE_URL} />
       <h4>{name}</h4>
       <p>{cuisines}</p>
       <h4>{avgRating}</h4>
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,2 +1,5 @@
 export const LOGO_URL =
   "https://cdn.vectorstock.com/i/500p/73/82/food-logo-vector-38377382.jpg";
+
+export const CARD_IMAGE_URL =
+  "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg";
