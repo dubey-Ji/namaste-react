17c7f5742f298035b5cf1503505a7332a84e83f1
Ashutosh Dubey <ashd1014@hotmail.com>
2024-07-08 09:49:12 +0530

    using restuarant url as configured

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -5,3 +5,6 @@ export const CARD_IMAGE_URL =
   "https://img.freepik.com/free-photo/tasty-burger-isolated-white-background-fresh-hamburger-fastfood-with-beef-cheese_90220-1063.jpg";
 
 export const TOP_RATING_THRESHOLD = 4.0;
+
+export const RESTUARANT_MENU_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId=";
